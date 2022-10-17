80f47694cee11a36f9c84c18a167dc4919779b89
Brendan Van Hook <brendan@vastactive.com>
2022-10-17 14:10:39 -0400

    Begin tracking neovim with home-manager. Start from scratch with only settings I know I use.

--- nix/home.nix
+++ nix/home.nix
@@ -6,22 +6,47 @@ in
 {
   home.username = "${user}";
   home.homeDirectory = "/home/${user}";
-  programs = {
-    home-manager.enable = true;
-    neovim = {
-      enable = true;
-      withPython3 = true;
-      viAlias = true;
-      vimAlias = true;
-    };
-    tmux = {
-      enable = true;
-      plugins = with pkgs.tmuxPlugins; [
-        sensible
-        vim-tmux-navigator
-      ];
-    };
+
+  programs.home-manager.enable = true;
+  programs.fish = {
+    enable = true;
+    functions = {
+      mkcd = "mkdir -p $argv[1] && cd $argv[1]";
+    }; 
   };
+  programs.neovim = {
+    enable = true;
+    withPython3 = true;
+    viAlias = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      vim-tmux-navigator
+    ];
+    extraConfig = ''
+      silent !mkdir -p ~/.local/share/nvim/undo
+      silent !mkdir -p ~/.local/share/nvim/backup
+      silent !mkdir -p ~/.cache/nvim/backup
+      set relativenumber
+      set number
+      set undofile
+      set backup
+      set undodir=~/.local/share/nvim/undo//
+      set backupdir=~/.local/share/nvim/backup//
+      set directory=~/.cache/nvim/swap//
+    '';
+  };
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      vim-tmux-navigator
+    ];
+  } ;
 
   home.packages = with pkgs; [
     cachix
