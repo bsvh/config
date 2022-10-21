1e85bb5b88931bcbb204b61a7f5c077b6a34bda4
Brendan Van Hook <brendan@vastactive.com>
2022-10-21 19:37:21 -0400

    Setup bash.

---
+++ bash_custom.sh
@@ -0,0 +1,21 @@
+# Setup nix environment
+if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
+	source $HOME/.nix-profile/etc/profile.d/nix.sh
+	export XDG_DATA_DIRS="$HOME/.nix-profile/share/:$XDG_DATA_DIRS"
+fi
+if [ -e $HOME/.nix-profile/etc/profile.d/bash_completion.sh ]; then
+	source $HOME/.nix-profile/etc/profile.d/bash_completion.sh
+fi
+
+# Enable GnuPG SSH
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+	export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+export GPG_TTY=$(tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
+
+# Execute fish for interactive shells
+if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && $(which fish 2>/dev/null) && -z ${BASH_EXECUTION_STRING} ]]; then
+	exec fish
+fi
