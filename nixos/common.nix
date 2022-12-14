99ef1855a4febba8f4bf632c5af163233f00bb27
Brendan Van Hook <brendan@vastactive.com>
2022-12-14 18:42:57 -0500

    Fix pipewire/pulseaudio.

--- nixos/common.nix
+++ nixos/common.nix
@@ -4,6 +4,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "America/New_York";
 
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
