5d0a2f5781b6100f6bd3d2376c8894c270f55518
Brendan Van Hook <brendan@vastactive.com>
2022-12-16 16:35:07 -0500

    Remove gnome.

--- nixos/ubik.nix
+++ nixos/ubik.nix
@@ -85,7 +85,6 @@
   networking.networkmanager.enable = true;
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
   
   environment.systemPackages = with pkgs; [
     helix
