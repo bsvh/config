e75ae86400dc14da4e01735f0df44cd6cb1b78af
Brendan Van Hook <brendan@vastactive.com>
2022-12-15 20:43:50 -0500

    Setup xdg directories.

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -16,6 +16,18 @@ in
     };
   };
 
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "${config.home.homeDirectory}/desktop";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/downloads";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/pictures";
+    videos = "${config.home.homeDirectory}/videos";
+    templates = "${config.home.homeDirectory}/templates";
+    publicShare = "${config.home.homeDirectory}/public";
+  };
 
   home.packages = with pkgs; [
     anki
