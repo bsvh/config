7e6fee78e866ce102684166e918bc2c2238bae73
Brendan Van Hook <brendan@vastactive.com>
2023-01-08 17:28:06 -0500

    Add neovim setup.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,8 +55,8 @@ in
     publicShare = "${config.home.homeDirectory}/public";
   };
   home.shellAliases = {
-    vim = "hx";
-    nvim = "hx";
+    vi = "nvim";
+    vim = "nvim";
   };
   home.packages = with pkgs; [
     bacon
@@ -113,6 +113,115 @@ in
         '';
       };
     };
+  };
+  programs.neovim = {
+    enable = true;
+    coc.enable = true;
+    coc.settings = {
+      "coc.preferences.formatOnSaveFiletypes" = [ "rust" "c" "json" ];
+    };
+    plugins = with pkgs.vimPlugins; [
+      vim-monokai-pro
+      vim-tmux-navigator
+
+      # Languages
+      coc-clangd
+      coc-css
+      coc-lua
+      coc-json
+      coc-html
+      coc-pyright
+      coc-rust-analyzer
+      coc-sh
+      coc-toml
+      coc-yaml
+      vim-nix
+
+      # GUI
+      coc-fzf
+      fzf-vim
+      
+      
+    ];
+    extraPackages = with pkgs; [
+      clang
+      clang-tools
+    ];
+    extraConfig = ''
+      set relativenumber
+      set number
+      set splitbelow
+      set splitright
+      set nowrap
+      set signcolumn=yes
+      set tabstop=4 shiftwidth=4 softtabstop=4
+
+      """ Colorscheme
+      colorscheme monokai_pro
+      hi Normal guibg=NONE ctermbg=NONE
+      hi LineNr guibg=NONE ctermbg=NONE
+      hi SignColumn guibg=NONE ctermbg=NONE
+      hi Comment cterm=italic
+      hi! link CocInlayHint Comment
+      hi! link EndOfBuffer Normal
+
+      set incsearch ignorecase smartcase hlsearch
+
+      map <Space> <Leader>
+
+      " FZF
+      nmap <leader><tab> <plug>(fzf-maps-n)
+      noremap <silent> <leader>ff :Files<CR>
+      noremap <silent> <leader>fb :Buffers<CR>
+      noremap <silent> <leader>fp :Files ~/projects<CR>
+      noremap <silent> <leader>gf :GFiles<CR>
+      noremap <silent> <leader>gs :GFiles?<CR>
+      noremap <silent> <leader>gc :Commits<CR>
+      noremap <silent> <leader>gb :BCommits<CR>
+      noremap <leader>sr :Rg      
+      
+      " Space unhighlights search
+      noremap <silent> <leader><Space> :silent noh<Bar>echo<CR>
+
+      """ Coc
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)  
+      nmap <leader>rn <Plug>(coc-rename)
+      xmap <leader>q  <Plug>(coc-format-selected)
+      nmap <leader>q  <Plug>(coc-format-selected)
+      " Mappings for CoCList
+      " Show all diagnostics
+      nnoremap <silent><nowait> <leader>ca  :<C-u>CocList diagnostics<cr>
+      " Manage extensions
+      nnoremap <silent><nowait> <leader>ce  :<C-u>CocList extensions<cr>
+      " Show commands
+      nnoremap <silent><nowait> <leader>cc  :<C-u>CocList commands<cr>
+      " Find symbol of current document
+      nnoremap <silent><nowait> <leader>co  :<C-u>CocList outline<cr>
+      " Search workspace symbols
+      nnoremap <silent><nowait> <leader>ss  :<C-u>CocList -I symbols<cr>
+      " Do default action for next item
+      nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
+      " Do default action for previous item
+      nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
+
+      autocmd CursorHold * silent call CocActionAsync('highlight')
+      set statusline^=%{coc#status()}%{get(b:,'coc_current_function',''')}
+
+      inoremap <silent><expr> <TAB>
+            \ coc#pum#visible() ? coc#pum#next(1) :
+            \ CheckBackspace() ? "\<Tab>" :
+            \ coc#refresh()
+      inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+            inoremap <expr> <TAB> pumvisible() ? "\<C-y>" : "\<C-g>u\<TAB>"
+
+      """ Language Specific Formatting
+      autocmd FileType xml setlocal tabstop=2 shiftwidth=2
+      autocmd FileType xml setlocal tabstop=2 shiftwidth=2
+      autocmd FileType c,cpp,h,hpp  setlocal tabstop=8 shiftwidth=8 
+    '';
   };
   programs.nix-index = {
     enable = true;
